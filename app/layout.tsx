7c7447deb1e88e1b751f69da8eb0b1108fb88609
Gideon Abe <gideonabe2020@gmail.com>
2026-02-02 19:45:33 +0100

    fixed a bug

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from '@vercel/analytics/react';
 import "./globals.css";
 import { Inter } from 'next/font/google'
 
@@ -25,6 +26,7 @@ export default function RootLayout({
         className={`${inter.variable} ${inter.variable} antialiased`}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
